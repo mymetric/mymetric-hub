2e726daf7c55465acf09e3e4eee01115ebc49136
Thiago Morello <thiago@mymetric.com.br>
2025-09-01 11:56:53 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true,
+    },
+  },
+  optimizeDeps: {
+    include: ['recharts'],
+  },
 }) 
